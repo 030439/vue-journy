58ff0ddd8b7af2bae109e6c4afd51ee4d31e3bf5
https://github.com/030439 <praimcahnd763@gmail.com>
2023-12-19 02:44:44 +0500

    text and color set into props for dynamic content into button comp

--- taskTracker/tasktracker/src/components/PageHeader.vue
+++ taskTracker/tasktracker/src/components/PageHeader.vue
@@ -1,16 +1,21 @@
 <template>
     <div>
       <h1>{{ title}}</h1>
+      <TaskButton />
       <!-- Other content goes here -->
     </div>
   </template>
   
   <script>
+  import TaskButton from './TaskButton'
   export default {
     name: 'PageHeader',
     props:{
         title:String,
     },
+    components:{
+        TaskButton,
+    }
   };
   </script>
   
---
+++ taskTracker/tasktracker/src/components/TaskButton.vue
@@ -0,0 +1,12 @@
+<template>
+<button class="btn">Add Task</button>
+</template>
+<script>
+export default{
+    name:'TaskButton',
+    props:{
+        text:String,
+        color:String,
+    }
+}
+</script>
